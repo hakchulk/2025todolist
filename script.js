c4cfc5b4b115cd6fda0b61b42089b4b11344bfb7
hakchulk <hakchulk@gmail.com>
2025-09-23 12:46:11 +0900

    취소버튼

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let listView = [
-  { todo: "자바11111111111111111111111111111", complete: false },
-  { todo: "자바 스크립트", complete: true },
-  { todo: "오늘 할일", complete: false },
-];
+// let listView = [
+//   { todo: "자바", complete: false },
+//   { todo: "자바 스크립트", complete: true },
+//   { todo: "오늘 할일", complete: false },
+// ];
+
+function init() {
+  let sListView = localStorage.getItem("todoData");
+  if (sListView) {
+    listView = JSON.parse(sListView);
+  } else {
+    listView = [];
+  }
+  viewList();
+}
+
+function saveList() {
+  localStorage.setItem("todoData", JSON.stringify(listView));
+}
 
 function getInnerText(item, idx) {
   let s = `<li class="d-flex py-2">
-        <div class="col-8 justify-content-between ${
+        <div class="col-9 justify-content-between ${
           item.complete ? "text-decoration-line-through" : ""
         }" >${item.todo}</div>
         <div class="d-flex gap-3">
@@ -30,8 +44,13 @@ function modiItem(idx) {
   txtInp.value = listView[idx].todo;
 
   txtInp.focus();
+  toggleBtns();
+}
+
+function toggleBtns() {
   btnInp.classList.toggle("d-none");
   btnModi.classList.toggle("d-none");
+  btnCancel.classList.toggle("d-none");
 }
 
 function viewList() {
@@ -52,10 +71,12 @@ function viewList() {
 
 function done(idx) {
   listView[idx].complete = !listView[idx].complete;
+  saveList();
   viewList();
 }
 
 function delItem(idx) {
   listView.splice(idx, 1);
+  saveList();
   viewList();
 }
